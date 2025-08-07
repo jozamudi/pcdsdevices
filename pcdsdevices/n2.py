8831b85f86405825734d7eb2a05240ceaf0860b1
Josue Zamudio <jozamudi@slac.stanford.edu>
2025-08-07 11:25:14 -0700

    add missing pvs

--- pcdsdevices/n2.py
+++ pcdsdevices/n2.py
@@ -12,20 +12,60 @@ class N2(BaseInterface, Device):
     """
     mass = Cpt(PytmcSignal,
                ':MASS',
+               io='io',
                kind='normal',
-               doc='Mass of the N2 in kg')
+               doc='Mass of the N2 in lbs')
 
     pressure = Cpt(PytmcSignal,
                    ':PRESSURE',
+                   io='io',
                    kind='normal',
-                   doc='Pressure of the N2 in Pa')
+                   doc='Pressure of the N2 in psi')
 
     start_exchange = Cpt(PytmcSignal,
                          ':START_EXCHANGE',
+                         io='io',
                          kind='normal',
                          doc='Start N2 exchange')
 
     finish_exchange = Cpt(PytmcSignal,
                           ':FINISH_EXCHANGE',
+                          io='io',
                           kind='normal',
                           doc='Finish N2 exchange')
+
+    max_mass = Cpt(PytmcSignal,
+                   ':MAX_MASS',
+                   io='io',
+                   kind='normal',
+                   doc='Maximum allowable mass of N2 in lbs')
+
+    min_mass = Cpt(PytmcSignal,
+                   ':MIN_MASS',
+                   io='io',
+                   kind='normal',
+                   doc='Minimum allowable mass of N2 in lbs')
+
+    min_mass_threshold = Cpt(PytmcSignal,
+                             ':MIN_MASS_THRESHOLD',
+                             io='io',
+                             kind='normal',
+                             doc='Minimum mass threshold in lbs')
+
+    min_pressure_threshold = Cpt(PytmcSignal,
+                                 ':MIN_PRESS_THRESHOLD',
+                                 io='io',
+                                 kind='normal',
+                                 doc='Minimum pressure threshold in psi')
+
+    warning_mass_threshold = Cpt(PytmcSignal,
+                                 ':MIN_MASS_WARNING_THRESHOLD',
+                                 io='io',
+                                 kind='normal',
+                                 doc='Warning if mass is less than threshold in lbs')
+
+    led_status = Cpt(PytmcSignal,
+                     ':LED',
+                     io='io',
+                     kind='normal',
+                     doc='LED status indicator')
